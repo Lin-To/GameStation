98f5b5f8de8dbf39f0e670e131c4789df287ba9c
Sally Li <sallyli0410@gmail.com>
2019-07-31 12:07:27 +0800

    update

--- app/controllers/api/v1/login_controller.rb
+++ app/controllers/api/v1/login_controller.rb
@@ -1,22 +1,25 @@
 class Api::V1::LoginController < Api::V1::BaseController
+  # skip_before_action :authenticate_user!
+  skip_before_action :verify_authenticity_token
+
   URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
   def wechat_user
-    params = {
+    wechat_params = {
       appId: ENV["appId"],
       secret: ENV["secret"],
       js_code: params[:code],
-      grant_type: authorization_code
+      grant_type: "authorization_code"
     }
-    @wechat_response ||= RestClient.get(URL, params: params)
+
+    @wechat_response ||= RestClient.post(URL, wechat_params)
     @wechat_user ||= JSON.parse(@wechat_response.body)
   end
 
   def login
+    p 'I am logging in'
     @user = User.find_or_create_by(open_id: wechat_user.fetch("openid"))
     render json: {
-      userId: @user.id,
-      userName: @user.name,
-      userDescription: @user.description
+      userId: @user.id
     }
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,3 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :authenticate_user!
 end
